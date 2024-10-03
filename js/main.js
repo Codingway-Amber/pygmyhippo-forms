d833504986b1239670990817bc716b716def7667
Codingway-Amber <amberreine7@gmail.com>
2024-10-03 10:21:26 -0500

    console practice

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
-// JS scripts placed here
+// // JS scripts placed here
+// const myButton = document.getElementById("submit"); 
+// console.log(myButton); 
+
+// const element = document.getElementsByTagName("p"); 
+// console.log(element); 
+
+// const x = document.querySelectorAll(".logo");
+// console.log(x); 
+
+// const y = document.querySelector(".logo");
+// console.log(y);
