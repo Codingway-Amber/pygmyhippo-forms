348277c03f1af7a4c06bd45acb4bdedca7a9589e
Codingway-Amber <amberreine7@gmail.com>
2024-10-08 10:58:34 -0500

    mobile layout practice

--- js/main.js
+++ js/main.js
@@ -1,12 +1,33 @@
-// // JS scripts placed here
-// const myButton = document.getElementById("submit"); 
-// console.log(myButton); 
 
-// const element = document.getElementsByTagName("p"); 
-// console.log(element); 
-
-// const x = document.querySelectorAll(".logo");
-// console.log(x); 
-
-// const y = document.querySelector(".logo");
-// console.log(y);
+// function handleClick(ev) {
+//     // Prevent the default action of the event
+//     ev.preventDefault();
+//     // declare variables for use
+//     const body = document.querySelector('body');
+//     // Get the value of the input field
+//     const nameInput = document.getElementById('name');
+//     const nameValue = nameInput.value;
+//     console.log('nameValue:', nameValue);
+//     alert('You clicked me!, ' + nameValue);
+//     // Perform action: add class to body element
+//     body.classList.add('form-submitted');
+// }
+const button = document.getElementById('submit');
+button.addEventListener("click", (ev) => {
+    // Prevent the default action of the event
+    ev.preventDefault();
+    // declare variables for use
+    const body = document.querySelector('body');
+    const nameOutput = document.getElementById('form-name');
+    console.log(nameOutput);
+    // Get the value of the input field
+    const nameInput = document.getElementById('name');
+    const nameValue = nameInput.value;
+    console.log('nameInput:', nameInput);
+    console.log('nameValue:', nameValue);
+    nameOutput.innerHTML = nameValue;
+    // alert('You clicked me!, ' + nameValue);
+    // Perform action: add class to body element
+    body.classList.add('form-submitted');
+    window.scrollTo(0, 0);
+});
